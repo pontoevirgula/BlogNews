b1ca62a4348d7ef790635b6c3784fcfc8555f755
Carlos Henrique <chsl200293@gmail.com>
2019-12-11 18:46:19 -0300

    criado layout de detalhes do post

---
+++ app/src/main/java/com/chsltutorials/blognews/activity/PostDetailsActivity.kt
@@ -0,0 +1,13 @@
+package com.chsltutorials.blognews.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.chsltutorials.blognews.R
+
+class PostDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post_details)
+    }
+}
