0fb5bb0971b7647839ef6739a5f9f1d3d0d183f8
Carlos Henrique <chsl200293@gmail.com>
2019-12-11 22:51:44 -0300

    criado activity de detalhes que bind post data

--- app/src/main/java/com/chsltutorials/blognews/activity/PostDetailsActivity.kt
+++ app/src/main/java/com/chsltutorials/blognews/activity/PostDetailsActivity.kt
@@ -1,13 +1,56 @@
 package com.chsltutorials.blognews.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import android.text.format.DateFormat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.chsltutorials.blognews.R
+import com.chsltutorials.blognews.base.BaseActivity
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_post_details.*
+import java.util.*
 
-class PostDetailsActivity : AppCompatActivity() {
+
+class PostDetailsActivity : BaseActivity() {
+
+    lateinit var postKey : String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_post_details)
+
+        //deixar barra de status transparente
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        supportActionBar!!.hide()
+
+
+        mAuth = FirebaseAuth.getInstance()
+        currentUser = FirebaseAuth.getInstance().currentUser!!
+
+        intent?.extras?.let {
+            val postImage = it.getString("pictures")
+            Glide.with(this).load(postImage).into(ivPostDetail)
+
+            val userPhotoPost = it.getString("userPhoto")
+            Glide.with(this).load(userPhotoPost).apply(RequestOptions.circleCropTransform()).into(ivPostDetailUserPhoto)
+
+            Glide.with(this).load(currentUser.photoUrl).apply(RequestOptions.circleCropTransform()).into(ivPostDetailCurrentUser)
+
+            tvPostTitle.text = it.getString("title")
+
+            tvPostDetailDesc.text = it.getString("description")
+
+            postKey = it.getString("key")!!
+
+            val date = timestampToString(it.getLong("date"))
+            tvPostDetailDate.text = date
+        }
+    }
+
+    private fun timestampToString(time: Long): String? {
+        val calendar = Calendar.getInstance(Locale.US)
+        calendar.timeInMillis = time
+        return DateFormat.format("dd-MM-yyyy", calendar).toString()
     }
 }
--- app/src/main/java/com/chsltutorials/blognews/adapter/PostAdapter.kt
+++ app/src/main/java/com/chsltutorials/blognews/adapter/PostAdapter.kt
@@ -1,6 +1,7 @@
 package com.chsltutorials.blognews.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,6 +9,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.chsltutorials.blognews.R
+import com.chsltutorials.blognews.activity.PostDetailsActivity
 import com.chsltutorials.blognews.model.Post
 import kotlinx.android.synthetic.main.adapter_post_item.view.*
 
@@ -19,11 +21,27 @@ class PostAdapter(var context: Context, var posts : MutableList<Post>) : Recycle
     override fun getItemCount() = posts.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.itemView.tvRowTitle.text = posts[position].title
+        holder.itemView.tvRowPostTitle.text = posts[position].title
         Glide.with(context).load(posts[position].pictures).into(holder.itemView.ivRowPost)
         Glide.with(context).load(posts[position].userPhoto).apply(RequestOptions.circleCropTransform()).into(holder.itemView.ivRowPostProfile)
     }
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+        init {
+            itemView.setOnClickListener {
+                val intentPostDetailsActivity = Intent(it.context,PostDetailsActivity::class.java)
+                intentPostDetailsActivity.putExtra("key",posts[adapterPosition].postKey)
+                intentPostDetailsActivity.putExtra("title",posts[adapterPosition].title)
+                intentPostDetailsActivity.putExtra("description",posts[adapterPosition].description)
+                intentPostDetailsActivity.putExtra("pictures",posts[adapterPosition].pictures)
+                intentPostDetailsActivity.putExtra("userPhoto",posts[adapterPosition].userPhoto)
+                intentPostDetailsActivity.putExtra("date",posts[adapterPosition].timestamp as Long)
+                //TODO SERA ADD DEPOIS "USERNAME" NO OBJETO POST
+                //intentPostDetailsActivity.putExtra("userName",posts[adapterPosition].userName)
+                context.startActivity(intentPostDetailsActivity)
+            }
+        }
+    }
 }
 
